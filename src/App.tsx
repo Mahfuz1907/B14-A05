b2e902a0d808a0829e6df9998d5b881ab7364342
Mahfuz1907 <mahfuztamim1907@gmail.com>
2026-09-10 23:55:11 +0600

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Banner from './Components/Banner/Banner'
+import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Banner />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,51 @@
+import '../../App.css';
+
+function Footer() {
+    return (
+        <div className='border border-[#f1f5f9] bg-white px-25 pt-16 pb-12 flex flex-col justify-between items-start gap-14'>
+            <div className='flex flex-row justify-start items-start gap-10 lg:gap-40'>
+                <div className='flex flex-col justify-between items-start gap-3'>
+                    <img src="/assets/logo-text.png" alt="" />
+                    <p className='text-[#64748b] pjs text-xs font-normal'>Curated tools, technologies, and resources for developers building modern software.</p>
+                    <ul className='flex flex-row justify-start items-center gap-4 text-[#475569] pjs text-xs font-semibold'>
+                        <li className='cursor-pointer hover:text-[#d91b7e]'>GitHub</li>
+                        <li className='cursor-pointer hover:text-[#d91b7e]'>Twitter</li>
+                        <li className='cursor-pointer hover:text-[#d91b7e]'>LinkedIn</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col justify-between items-start gap-4'>
+                    <h1 className='text-[#0f172a] pjs text-xs font-bold'>PRODUCT</h1>
+                    <ul className='flex flex-col justify-between items-start gap-2.5 pjs text-[#64748b] text-xs font-normal'>
+                        <li className='cursor-pointer hover:text-black'>Home</li>
+                        <li className='cursor-pointer hover:text-black'>Technologies</li>
+                        <li className='cursor-pointer hover:text-black'>Projects</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col justify-between items-start gap-4'>
+                    <h1 className='text-[#0f172a] pjs text-xs font-bold'>COMPANY</h1>
+                    <ul className='flex flex-col justify-between items-start gap-2.5 pjs text-[#64748b] text-xs font-normal'>
+                        <li className='cursor-pointer hover:text-black'>About</li>
+                        <li className='cursor-pointer hover:text-black'>Contact</li>
+                        <li className='cursor-pointer hover:text-black'>Careers</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col justify-between items-start gap-4'>
+                    <h1 className='text-[#0f172a] pjs text-xs font-bold'>LEGAL</h1>
+                    <ul className='flex flex-col justify-between items-start gap-2.5 pjs text-[#64748b] text-xs font-normal'>
+                        <li className='cursor-pointer hover:text-black'>Privacy Policy</li>
+                        <li className='cursor-pointer hover:text-black'>Terms of Service</li>
+                    </ul>
+                </div>
+            </div>
+            <div className='border border-[#f1f5f9] pt-8 pjs text-xs font-normal text-[#94a3b8] flex flex-row justify-between items-end w-full'>
+                <p>© 2026 Dev Stack. All rights reserved.</p>
+                <ul className='flex flex-row justify-end items-center gap-6'>
+                    <li className='cursor-pointer hover:text-black'>Privacy</li>
+                    <li className='cursor-pointer hover:text-black'>Terms</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
